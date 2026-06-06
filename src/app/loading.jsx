42fc2d7e61c39edd6a97db4d99e1996550a39a81
sanjana-moon <sanjanamoonliya@gmail.com>
2026-06-06 14:56:49 +0600

    readme.md file added

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -8,7 +8,7 @@ const loading = () => {
         <div className="min-h-[80vh] flex flex-col items-center justify-center gap-4">
             <FadeLoader color={PRIMARY_COLOR} />
             <p className="text-sm text-gray-500">
-                Loading your bookings...
+                Loading...
             </p>
         </div>
     );
